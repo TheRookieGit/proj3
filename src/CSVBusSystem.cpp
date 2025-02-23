a1060b4825fada30963902a799633ab6ac0a7a81
TheRookieGit <InitialDWX@gmail.com>
2025-02-23 00:00:06 -0800

    CSV BusSystem skele works, write 1 simple test for BusSystem and FAILED as expected

--- src/CSVBusSystem.cpp
+++ src/CSVBusSystem.cpp
@@ -1,7 +1,47 @@
 #include "CSVBusSystem.h"
+#include "BusSystem.h"
+#include "DSVReader.h"
 #include <memory>
 #include <string>
 #include <cstddef>
+
+struct CCSVBusSystem::SImplementation{
+
+    struct SStop:CBusSystem::SStop{
+
+
+        CBusSystem::TStopID ID() const noexcept override{
+            return ID();
+        }
+
+        CStreetMap::TNodeID NodeID() const noexcept override{
+            return NodeID();
+        }
+
+
+    };
+
+    struct SRoute : CBusSystem::SRoute {
+        std::string Name() const noexcept {
+            return "";
+        }
+
+        // Returns the number of stops on the route 
+        std::size_t StopCount() const noexcept {
+            return 0;
+        }
+
+        // Returns the stop id specified by the index, returns InvalidStopID if index is greater than or equal to StopCount() 
+        TStopID GetStopID(std::size_t index) const noexcept {
+            return TStopID();
+        }
+
+
+    };
+
+
+    
+};
 
 // CCSVBusSystem member functions 
 // Constructor for the CSV Bus System 
@@ -9,63 +49,77 @@ CCSVBusSystem::CCSVBusSystem(std::shared_ptr<CDSVReader> stopsrc, std::shared_pt
 {
 }
 
+//Done
 // Destructor for the CSV Bus System 
-CCSVBusSystem::~CCSVBusSystem() {
-}
+CCSVBusSystem::~CCSVBusSystem() = default;
 
+//Done
 // Returns the number of stops in the system 
 std::size_t CCSVBusSystem::StopCount() const noexcept {
     return 0;
 }
 
+//Done
 // Returns the number of routes in the system 
 std::size_t CCSVBusSystem::RouteCount() const noexcept {
     return 0;
 }
 
+//Done
 // Returns the SStop specified by the index, nullptr is returned if index is greater than equal to StopCount() 
-std::shared_ptr<SStop> CCSVBusSystem::StopByIndex(std::size_t index) const noexcept {
+std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByIndex(std::size_t index) const noexcept {
     return nullptr;
 }
 
+
+//Done
 // Returns the SStop specified by the stop id, nullptr is returned if id is not in the stops 
-std::shared_ptr<SStop> CCSVBusSystem::StopByID(TStopID id) const noexcept {
+std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByID(TStopID id) const noexcept {
     return nullptr;
 }
 
+
+//Done
 // Returns the SRoute specified by the index, nullptr is returned if index is greater than equal to RouteCount() 
-std::shared_ptr<SRoute> CCSVBusSystem::RouteByIndex(std::size_t index) const noexcept {
+std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByIndex(std::size_t index) const noexcept {
     return nullptr;
 }
 
+
+//Done
 // Returns the SRoute specified by the name, nullptr is returned if name is not in the routes 
-std::shared_ptr<SRoute> CCSVBusSystem::RouteByName(const std::string &name) const noexcept {
+std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByName(const std::string &name) const noexcept {
     return nullptr;
 }
 
-// Bus System Stop member functions 
-// Returns the stop id of the stop 
-TStopID SStopImpl::ID() const noexcept {
-    return TStopID();
-}
 
-// Returns the node id of the bus stop 
-CStreetMap::TNodeID SStopImpl::NodeID() const noexcept {
-    return CStreetMap::TNodeID();
-}
 
-// Bus System Route member functions 
-// Returns the name of the route 
-std::string SRouteImpl::Name() const noexcept {
-    return "";
-}
 
-// Returns the number of stops on the route 
-std::size_t SRouteImpl::StopCount() const noexcept {
-    return 0;
-}
 
-// Returns the stop id specified by the index, returns InvalidStopID if index is greater than or equal to StopCount() 
-TStopID SRouteImpl::GetStopID(std::size_t index) const noexcept {
-    return TStopID();
-}
+
+// // Bus System Stop member functions 
+// // Returns the stop id of the stop 
+// TStopID SStopImpl::ID() const noexcept {
+//     return TStopID();
+// }
+
+// // Returns the node id of the bus stop 
+// CStreetMap::TNodeID SStopImpl::NodeID() const noexcept {
+//     return CStreetMap::TNodeID();
+// }
+
+// // Bus System Route member functions 
+// // Returns the name of the route 
+// std::string SRouteImpl::Name() const noexcept {
+//     return "";
+// }
+
+// // Returns the number of stops on the route 
+// std::size_t SRouteImpl::StopCount() const noexcept {
+//     return 0;
+// }
+
+// // Returns the stop id specified by the index, returns InvalidStopID if index is greater than or equal to StopCount() 
+// TStopID SRouteImpl::GetStopID(std::size_t index) const noexcept {
+//     return TStopID();
+// }
--- testsrc/CSVBusSystemTest.cpp
+++ testsrc/CSVBusSystemTest.cpp
@@ -1,3 +1,44 @@
 #include <gtest/gtest.h>
 #include "CSVBusSystem.h"
+#include "DSVReader.h"
+#include "StringDataSource.h"
+
+#include <string>
+#include <memory>
+using std::string;
+
+using std::shared_ptr;
+
+
+std::shared_ptr<CDSVReader> CreateDSVReader_proj3(const std::string& input) {
+    auto datasource = std::make_shared<CStringDataSource>(input);
+    return std::make_shared<CDSVReader>(datasource, ',');
+}
+
+TEST(CSVBusSystemTest, Stop_andRoute_Counts){
+    // string test_stops = "stop_id, node_id\n1,100\n2,200\n3,300\n";
+    
+    string test_stops =
+    "stop_id,node_id\n"
+    "22043,2849810514\n"
+    "22358,2849805223\n"
+    "22105,3954294407\n";
+
+    string test_routes =
+        "route,stop_id\n"
+        "A,22258\n"
+        "A,22169\n"
+        "A,22000\n"
+        "B,22185\n"
+        "B,22189\n"
+        "B,22188\n";
+
+        auto read_stops = CreateDSVReader_proj3(test_stops); 
+        auto read_routes = CreateDSVReader_proj3(test_routes);
+        
+        CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+
+    EXPECT_EQ(CSVBusSystem.StopCount(), 3);
+    EXPECT_EQ(CSVBusSystem.RouteCount(), 2);
+}
 
