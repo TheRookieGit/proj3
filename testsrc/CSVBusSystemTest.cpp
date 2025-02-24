c5dfa6c77c80260c21e84977eda990807a6b5c79
TheRookieGit <InitialDWX@gmail.com>
2025-02-24 00:39:34 -0800

    added TEST for OSMTest. Now we have 5 tests for BusSystem and 3 tests for OSM All tests have been commented until functions completed Ready to start writing functions

--- testsrc/CSVBusSystemTest.cpp
+++ testsrc/CSVBusSystemTest.cpp
@@ -56,89 +56,95 @@ std::shared_ptr<CDSVReader> CreateDSVReader_proj3(const std::string& input_file)
 //     EXPECT_EQ(CSVBusSystem.RouteCount(), 2);
 // }
 
-TEST(CSVBusSystemTest, Stop_andRoute_Counts){
+//! **************************************************************************
+//! Wait Until CSVBusSystem function complete then uncomment everything below
+//! Otherwise had segment fault
+//! **************************************************************************
 
-    auto read_stops = CreateDSVReader_proj3("data/stops.csv");
-    auto read_routes = CreateDSVReader_proj3("data/routes.csv");
 
-    CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+// TEST(CSVBusSystemTest, Stop_andRoute_Counts){
 
-    EXPECT_EQ(CSVBusSystem.StopCount(), 298);
-    //EXPECT_EQ(CSVBusSystem.RouteCount(), 394);
-    EXPECT_EQ(CSVBusSystem.RouteCount(), 17);
+//     auto read_stops = CreateDSVReader_proj3("data/stops.csv");
+//     auto read_routes = CreateDSVReader_proj3("data/routes.csv");
+
+//     CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+
+//     EXPECT_EQ(CSVBusSystem.StopCount(), 298);
+//     //EXPECT_EQ(CSVBusSystem.RouteCount(), 394);
+//     EXPECT_EQ(CSVBusSystem.RouteCount(), 17);
     
-}
+// }
 
-TEST(CSVBusSystemTest, StopIndex_andStopID){
-    auto read_stops = CreateDSVReader_proj3("data/stops.csv");
-    auto read_routes = CreateDSVReader_proj3("data/routes.csv");
+// TEST(CSVBusSystemTest, StopIndex_andStopID){
+//     auto read_stops = CreateDSVReader_proj3("data/stops.csv");
+//     auto read_routes = CreateDSVReader_proj3("data/routes.csv");
 
-    CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+//     CCSVBusSystem CSVBusSystem(read_stops, read_routes);
 
-    auto stop_index0 = CSVBusSystem.StopByIndex(0);
+//     auto stop_index0 = CSVBusSystem.StopByIndex(0);
 
-    EXPECT_EQ(stop_index0 -> ID(), 22043);
-    EXPECT_EQ(stop_index0 -> NodeID(), 2849810514);
+//     EXPECT_EQ(stop_index0 -> ID(), 22043);
+//     EXPECT_EQ(stop_index0 -> NodeID(), 2849810514);
 
-    auto stop_ID0 = CSVBusSystem.StopByID(22358);
+//     auto stop_ID0 = CSVBusSystem.StopByID(22358);
     
-    EXPECT_EQ(stop_ID0 -> ID(), 22358);
-    EXPECT_EQ(stop_ID0 -> NodeID(), 2849805223);
+//     EXPECT_EQ(stop_ID0 -> ID(), 22358);
+//     EXPECT_EQ(stop_ID0 -> NodeID(), 2849805223);
 
-}
+// }
 
-TEST(CSVBusSystemTest, null_index_andID){
+// TEST(CSVBusSystemTest, null_index_andID){
 
-    auto read_stops = CreateDSVReader_proj3("data/stops.csv");
-    auto read_routes = CreateDSVReader_proj3("data/routes.csv");
+//     auto read_stops = CreateDSVReader_proj3("data/stops.csv");
+//     auto read_routes = CreateDSVReader_proj3("data/routes.csv");
 
-    CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+//     CCSVBusSystem CSVBusSystem(read_stops, read_routes);
 
-    EXPECT_EQ(CSVBusSystem.StopByIndex(299), nullptr);
-    EXPECT_EQ(CSVBusSystem.StopByID(99999), nullptr);
+//     EXPECT_EQ(CSVBusSystem.StopByIndex(299), nullptr);
+//     EXPECT_EQ(CSVBusSystem.StopByID(99999), nullptr);
 
-    EXPECT_EQ(CSVBusSystem.RouteByName("FAKERoute"), nullptr);
-    EXPECT_EQ(CSVBusSystem.RouteByIndex(999), nullptr);
-}
+//     EXPECT_EQ(CSVBusSystem.RouteByName("FAKERoute"), nullptr);
+//     EXPECT_EQ(CSVBusSystem.RouteByIndex(999), nullptr);
+// }
 
-TEST(CSVBusSystemTest, RouteIndex_andRouteName){
-    auto read_stops = CreateDSVReader_proj3("data/stops.csv");
-    auto read_routes = CreateDSVReader_proj3("data/routes.csv");
+// TEST(CSVBusSystemTest, RouteIndex_andRouteName){
+//     auto read_stops = CreateDSVReader_proj3("data/stops.csv");
+//     auto read_routes = CreateDSVReader_proj3("data/routes.csv");
 
-    CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+//     CCSVBusSystem CSVBusSystem(read_stops, read_routes);
 
-    auto route_index0 = CSVBusSystem.RouteByIndex(0);
+//     auto route_index0 = CSVBusSystem.RouteByIndex(0);
     
-    EXPECT_EQ(route_index0 -> Name(), "A");
-    EXPECT_NE(route_index0 -> StopCount(), 0);
+//     EXPECT_EQ(route_index0 -> Name(), "A");
+//     EXPECT_NE(route_index0 -> StopCount(), 0);
 
 
-    auto route_nameA = CSVBusSystem.RouteByName("A");
+//     auto route_nameA = CSVBusSystem.RouteByName("A");
     
-    EXPECT_EQ(route_nameA -> Name(), "A");
-    EXPECT_NE(route_nameA -> StopCount(), 0);
-
-    
-    EXPECT_EQ(CSVBusSystem.RouteByIndex(395), nullptr);
-    EXPECT_EQ(CSVBusSystem.RouteByName("NonExistent"), nullptr);
-
-}
-
-
-TEST(CSVBusSystemTest, Route_StopCounts) {
-    auto read_stops = CreateDSVReader_proj3("data/stops.csv");
-    auto read_routes = CreateDSVReader_proj3("data/routes.csv");
-    
-    CCSVBusSystem CSVBusSystem(read_stops, read_routes);
-
-    auto route_nameA = CSVBusSystem.RouteByName("A");
-
-    EXPECT_EQ(route_nameA -> StopCount(), 22);
-    EXPECT_EQ(route_nameA -> GetStopID(0), 22258);
-
-    auto stop_byID = CSVBusSystem.StopByID(22258);
-
-    EXPECT_EQ(stop_byID -> ID(), 22258);
+//     EXPECT_EQ(route_nameA -> Name(), "A");
+//     EXPECT_NE(route_nameA -> StopCount(), 0);
 
     
-}
+//     EXPECT_EQ(CSVBusSystem.RouteByIndex(395), nullptr);
+//     EXPECT_EQ(CSVBusSystem.RouteByName("NonExistent"), nullptr);
+
+// }
+
+
+// TEST(CSVBusSystemTest, Route_StopCounts) {
+//     auto read_stops = CreateDSVReader_proj3("data/stops.csv");
+//     auto read_routes = CreateDSVReader_proj3("data/routes.csv");
+    
+//     CCSVBusSystem CSVBusSystem(read_stops, read_routes);
+
+//     auto route_nameA = CSVBusSystem.RouteByName("A");
+
+//     EXPECT_EQ(route_nameA -> StopCount(), 22);
+//     EXPECT_EQ(route_nameA -> GetStopID(0), 22258);
+
+//     auto stop_byID = CSVBusSystem.StopByID(22258);
+
+//     EXPECT_EQ(stop_byID -> ID(), 22258);
+
+    
+// }
--- testsrc/OpenStreetMapTest.cpp
+++ testsrc/OpenStreetMapTest.cpp
@@ -13,21 +13,109 @@ using std::string;
 
 using std::shared_ptr;
 
-std::shared_ptr<CXMLReader> CreateXMLReader_proj3(const std::string& input){
-    auto datasource = std::make_shared<CStringDataSource>(input);
+//added for ifstream and fileread
+#include <sstream>
+#include <fstream>
+
+std::shared_ptr<CXMLReader> CreateXMLReader_proj3(const std::string& input_file){
+    
+    // -----------------
+
+    std::ifstream reading_file(input_file);
+
+    std::string reading_content;
+    std::string reading_lines;
+
+    while(std::getline(reading_file, reading_lines)){
+        reading_content += reading_lines;
+        reading_content += "\n";
+    }
+    // above copyed from BusSystemTest
+
+    auto datasource = std::make_shared<CStringDataSource>(reading_content);
     return std::make_shared<CXMLReader>(datasource);
 }
 
+//! **************************************************************************
+//! Wait Until CSVBusSystem function complete then uncomment everything below
+//! Otherwise had segment fault
+//! **************************************************************************
 
-TEST(OpenStreetMapTest, Node_andWay_Counts){
-    auto datasource = std::make_shared<CStringDataSource>("davis.osm");
 
-    auto read_xmldata = std::make_shared<CXMLReader>(datasource);
 
-    //CXMLReader CreateXMLReader_proj3(datasource);
 
-    COpenStreetMap OpenStreetMap(read_xmldata);
+// TEST(OpenStreetMapTest, Node_andWay_Counts){
+//     //auto datasource = std::make_shared<CStringDataSource>("data/davis.osm");
 
-    EXPECT_EQ(OpenStreetMap.NodeCount(), 10259);
-    EXPECT_EQ(OpenStreetMap.WayCount(), 1644);
-}
+//     //auto read_xmldata = std::make_shared<CXMLReader>(datasource);
+
+//     auto read_xmldata = CreateXMLReader_proj3("data/davis.osm");
+
+//     //CXMLReader CreateXMLReader_proj3(datasource);
+
+//     COpenStreetMap OpenStreetMap(read_xmldata);
+
+//     EXPECT_EQ(OpenStreetMap.NodeCount(), 10259);
+//     EXPECT_EQ(OpenStreetMap.WayCount(), 1644);
+// }
+
+// TEST(OpenStreetMapTest, NodeIndex_andID){
+//     //auto datasource = std::make_shared<CStringDataSource>("data/davis.osm");
+
+//     //auto read_xmldata = std::make_shared<CXMLReader>(datasource);
+    
+//     auto read_xmldata = CreateXMLReader_proj3("data/davis.osm");
+
+//     //CXMLReader CreateXMLReader_proj3(datasource);
+
+//     COpenStreetMap OpenStreetMap(read_xmldata);
+
+//     auto node_index0 = OpenStreetMap.NodeByIndex(0);
+
+//     EXPECT_EQ(node_index0->ID(), 62208369);
+
+//     // EXPECT_EQ(node_index0->Location().first, 38.5178523);
+//     // EXPECT_EQ(node_index0->Location().second, -121.7712408);
+
+//     EXPECT_NEAR(node_index0->Location().first, 38.5178523, 1e-6);
+//     EXPECT_NEAR(node_index0->Location().second, -121.7712408, 1e-6);
+
+//     auto node_id0 = OpenStreetMap.NodeByID(62209104);
+
+//     EXPECT_EQ(node_id0->ID(), 62209104);
+
+    
+//     // EXPECT_EQ(node_index0->Location().first, 38.535052);
+//     // EXPECT_EQ(node_index0->Location().second, -121.7408606);
+
+
+//     EXPECT_NEAR(node_id0->Location().first, 38.535052, 1e-6);
+//     EXPECT_NEAR(node_id0->Location().second, -121.7408606, 1e-6);
+
+
+//     EXPECT_EQ(OpenStreetMap.NodeByID(9999999999), nullptr);
+// }
+
+
+// TEST(OpenStreetMapTest, Node_Attributes){
+//     //auto datasource = std::make_shared<CStringDataSource>("data/davis.osm");
+
+//     //auto read_xmldata = std::make_shared<CXMLReader>(datasource);
+    
+//     auto read_xmldata = CreateXMLReader_proj3("data/davis.osm");
+
+//     //CXMLReader CreateXMLReader_proj3(datasource);
+
+//     COpenStreetMap OpenStreetMap(read_xmldata);
+
+
+//     auto node_id0 = OpenStreetMap.NodeByID(62224641);
+
+//     EXPECT_TRUE(node_id0->HasAttribute("highway"));
+//     EXPECT_TRUE(node_id0->HasAttribute("ref"));
+
+
+
+//     EXPECT_EQ(node_id0->GetAttribute("highway"), "motorway_junction");
+//     EXPECT_EQ(node_id0->GetAttribute("ref"), "71");
+// }
