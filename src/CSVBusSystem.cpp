f78c59e4225a08c2e7739b723162817c90f0b1e4
TheRookieGit <InitialDWX@gmail.com>
2025-02-24 12:15:00 -0800

    complete redefine all variables starting function: read CSV 1. read stops 2. read routes

--- src/CSVBusSystem.cpp
+++ src/CSVBusSystem.cpp
@@ -7,39 +7,48 @@
 
 struct CCSVBusSystem::SImplementation{
 
-    struct new_SStop:CBusSystem::SStop{
+    struct ToSaveStop:CBusSystem::SStop{
 
+        CBusSystem::TStopID saved_stopid;
+        CStreetMap::TNodeID saved_nodeid;
 
         CBusSystem::TStopID ID() const noexcept override{
-            return ID();
+            return saved_stopid;
         }
 
         CStreetMap::TNodeID NodeID() const noexcept override{
-            return NodeID();
+            //return NodeID();
+            return saved_nodeid;
         }
 
 
     };
 
-    struct new_SRoute : CBusSystem::SRoute {
+    struct ToSaveRoute : CBusSystem::SRoute {
+
+
+        std::string route_name;
+        std::vector<CBusSystem::TStopID> route_stopid;
+
         std::string Name() const noexcept {
-            return "";
+            return route_name;
         }
 
         // Returns the number of stops on the route 
         std::size_t StopCount() const noexcept {
-            return 0;
+            return route_stopid.size();
         }
 
         // Returns the stop id specified by the index, returns InvalidStopID if index is greater than or equal to StopCount() 
         TStopID GetStopID(std::size_t index) const noexcept {
-            return TStopID();
+            return route_stopid[index];
         }
 
 
     };
 
-
+    std::vector<ToSaveStop> all_stops;
+    std::vector<ToSaveRoute> all_routes;
     
 };
 
@@ -47,6 +56,13 @@ struct CCSVBusSystem::SImplementation{
 // Constructor for the CSV Bus System 
 CCSVBusSystem::CCSVBusSystem(std::shared_ptr<CDSVReader> stopsrc, std::shared_ptr<CDSVReader> routesrc)
 {
+
+    // std::vector<ToSaveStop> all_stops;
+    // std::vector<ToSaveRoute> all_routes;
+
+    DImplementation 
+
+
 }
 
 //Done with null
@@ -56,39 +72,65 @@ CCSVBusSystem::~CCSVBusSystem() = default;
 //Done with null
 // Returns the number of stops in the system 
 std::size_t CCSVBusSystem::StopCount() const noexcept {
-    return 0;
+    return DImplementation -> all_stops.size();
 }
 
 //Done with null
 // Returns the number of routes in the system 
 std::size_t CCSVBusSystem::RouteCount() const noexcept {
-    return 0;
+    return DImplementation -> all_routes.size();
 }
 
-//Done with null
-// Returns the SStop specified by the index, nullptr is returned if index is greater than equal to StopCount() 
+
+// Returns the SStop specified by the index, nullptr is returned if index is
+// greater than equal to StopCount()
 std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByIndex(std::size_t index) const noexcept {
+    if (index < DImplementation -> all_stops.size()) {
+        return DImplementation -> all_stops[index];
+    }
     return nullptr;
 }
 
 
-//Done with null
-// Returns the SStop specified by the stop id, nullptr is returned if id is not in the stops 
+// Returns the SStop specified by the stop id, nullptr is returned if id is
+// not in the stops
 std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByID(TStopID id) const noexcept {
+    for (const auto& stop : DImplementation -> all_stops) {
+        if (all_stops -> ID() == id) {
+            return all_stops;
+        }
+    }
     return nullptr;
 }
 
 
-//Done with null
-// Returns the SRoute specified by the index, nullptr is returned if index is greater than equal to RouteCount() 
+//! Duplicated, same function
+
+// Returns the SRoute specified by the index, nullptr is returned if index is
+// greater than equal to RouteCount()
+
 std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByIndex(std::size_t index) const noexcept {
+    if (index < DImplementation -> all_routes.size()) {
+        return DImplementation -> all_routes[index];
+    }
     return nullptr;
 }
 
 
-//Done with null
-// Returns the SRoute specified by the name, nullptr is returned if name is not in the routes 
+
+
+
+
+
+// Returns the SRoute specified by the name, nullptr is returned if name is
+// not in the routes
+
 std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByName(const std::string &name) const noexcept {
+    for (const auto& route : DImplementation -> all_routes) {
+        if (all_routes -> Name() == name) {
+            return all_routes;
+        }
+    }
     return nullptr;
 }
 
