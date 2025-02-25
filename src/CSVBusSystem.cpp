c077401dd3c1dfcdf9af43b6893124492f45d258
TheRookieGit <InitialDWX@gmail.com>
2025-02-24 19:42:39 -0800

    BusSystem: move Read function inside CCSVBusSystem, need to fix "push_back" errors

--- src/CSVBusSystem.cpp
+++ src/CSVBusSystem.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <cstddef>
 
+#include <vector>
+#include <algorithm>
+
 struct CCSVBusSystem::SImplementation{
 
     struct ToSaveStop:CBusSystem::SStop{
@@ -41,7 +44,12 @@ struct CCSVBusSystem::SImplementation{
 
         // Returns the stop id specified by the index, returns InvalidStopID if index is greater than or equal to StopCount() 
         TStopID GetStopID(std::size_t index) const noexcept {
-            return route_stopid[index];
+            if (index < route_stopid.size()){
+                return route_stopid[index];
+            }
+
+            return CBusSystem::InvalidStopID;
+            
         }
 
 
@@ -54,8 +62,104 @@ struct CCSVBusSystem::SImplementation{
 };
 
 
-namespace SeparateRead_StopsRoutes{
-    void ReadStops(CCSVBusSystem* new_CSVBusSystem, std::shared_ptr<CDSVReader> stopsrc){
+// namespace{
+//     void ReadStops(CCSVBusSystem* new_CSVBusSystem, std::shared_ptr<CDSVReader> stopsrc){
+//         while(!stopsrc -> End()){
+//             std::vector<std::string> reading_row;
+
+//             if(stopsrc-> ReadRow(reading_row)){
+//                 if(reading_row.size() >=2){
+//                     auto current_stop= std::make_shared<CCSVBusSystem::SImplementation::ToSaveStop>();
+
+
+//                     current_stop->saved_stopid = reading_row[0];
+//                     current_stop->saved_nodeid = reading_row[1];
+
+//                     new_CSVBusSystem->DImplementation->all_stops.push_back(*current_stop);
+
+//                 }
+//             }
+//         }
+//     }
+
+
+//     void ReadRoutes(CCSVBusSystem* new_CSVBusSystem, std::shared_ptr<CDSVReader> routesrc){
+//         bool is_firstrow = true;
+
+//         while (!routesrc->End()){
+//             std::vector<std::string> reading_row;
+            
+
+//             if(is_firstrow){
+//                 if(!isdigit(reading_row[0][0])){
+//                     is_firstrow = false;
+
+//                     continue;
+//                 }
+
+
+//             }
+
+//             is_firstrow = false;
+
+//             std::string current_route_name = reading_row[0];
+
+
+//             bool sameroute_exist = false;
+
+//             for(std::size_t i = 0; i < new_CSVBusSystem->DImplementation->all_routes.size(); i++){
+//                 if (new_CSVBusSystem->DImplementation->all_routes[i].route_name == current_route_name){
+
+//                     for (std::size_t j = 1; j < reading_row.size(); j++){
+//                         std::vector <std::string>& stop_list = new_CSVBusSystem->DImplementation->all_routes[i].route_stopid;
+
+
+//                         std::string new_stopid = reading_row[j];
+//                         stop_list.push_back(new_stopid);
+//                     }
+
+//                     sameroute_exist = true;
+
+//                     break;
+//                 }
+//             }
+
+
+//             if(!sameroute_exist){
+//                 CCSVBusSystem::SImplementation::ToSaveRoute create_newroute;
+
+//                 create_newroute.route_name = current_route_name;
+
+//                 for (std::size_t j = 1; j< reading_row.size();j++){
+//                     create_newroute.route_stopid.push_back(reading_row[j]);
+
+//                 }
+
+//                 new_CSVBusSystem->DImplementation->all_routes.push_back(create_newroute);
+//             }
+
+
+
+
+//         }
+
+//     }
+// }
+
+// CCSVBusSystem member functions 
+// Constructor for the CSV Bus System 
+CCSVBusSystem::CCSVBusSystem(std::shared_ptr<CDSVReader> stopsrc, std::shared_ptr<CDSVReader> routesrc)
+{
+
+    // std::vector<ToSaveStop> all_stops;
+    // std::vector<ToSaveRoute> all_routes;
+
+    DImplementation = std::make_unique<SImplementation>();
+
+    // ReadStops(this, stopsrc);
+    // ReadRoutes(this, routesrc);
+
+
         while(!stopsrc -> End()){
             std::vector<std::string> reading_row;
 
@@ -67,18 +171,17 @@ namespace SeparateRead_StopsRoutes{
                     current_stop->saved_stopid = reading_row[0];
                     current_stop->saved_nodeid = reading_row[1];
 
-                    new_CSVBusSystem->DImplementation->all_stops.push_back(*current_stop);
+                    DImplementation->all_stops.push_back(*current_stop);
 
                 }
             }
         }
-    }
+    
 
 
-    void ReadRoutes(CCSVBusSystem* new_CSVBusSystem, std::shared_ptr<CDSVReader> routesrc){
         bool is_firstrow = true;
 
-        while (!routesrc->End()){
+        while(!routesrc->End()){
             std::vector<std::string> reading_row;
             
 
@@ -99,12 +202,12 @@ namespace SeparateRead_StopsRoutes{
 
             bool sameroute_exist = false;
 
-            for(std::size_t i = 0; i < new_CSVBusSystem->DImplementation->all_routes.size(); i++){
-                if (new_CSVBusSystem->DImplementation->all_routes[i].route_name == current_route_name){
+            for(std::size_t i = 0; i < DImplementation->all_routes.size(); i++){
+                if (DImplementation->all_routes[i].route_name == current_route_name){
 
                     for (std::size_t j = 1; j < reading_row.size(); j++){
-                        std::vector <std::string>& stop_list = new_CSVBusSystem->DImplementation->all_routes[i].route_stopid;
-
+                        //std::vector <std::string>& stop_list = DImplementation->all_routes[i].route_stopid;
+                        std::vector <CBusSystem::TStopID>& stop_list = DImplementation->all_routes[i].route_stopid;
 
                         std::string new_stopid = reading_row[j];
                         stop_list.push_back(new_stopid);
@@ -127,7 +230,7 @@ namespace SeparateRead_StopsRoutes{
 
                 }
 
-                new_CSVBusSystem->DImplementation->all_routes.push_back(create_newroute);
+                DImplementation->all_routes.push_back(create_newroute);
             }
 
 
@@ -135,18 +238,8 @@ namespace SeparateRead_StopsRoutes{
 
         }
 
-    }
-}
+    
 
-// CCSVBusSystem member functions 
-// Constructor for the CSV Bus System 
-CCSVBusSystem::CCSVBusSystem(std::shared_ptr<CDSVReader> stopsrc, std::shared_ptr<CDSVReader> routesrc)
-{
-
-    // std::vector<ToSaveStop> all_stops;
-    // std::vector<ToSaveRoute> all_routes;
-
-    DImplementation = std::make_unique<SImplementation>();
 
     // //read stops
 
@@ -170,8 +263,8 @@ CCSVBusSystem::CCSVBusSystem(std::shared_ptr<CDSVReader> stopsrc, std::shared_pt
 
     //read routes
 
-
 }
+
 
 //Done with null
 // Destructor for the CSV Bus System 
@@ -194,7 +287,7 @@ std::size_t CCSVBusSystem::RouteCount() const noexcept {
 // greater than equal to StopCount()
 std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByIndex(std::size_t index) const noexcept {
     if (index < DImplementation -> all_stops.size()) {
-        return DImplementation -> all_stops[index];
+        return std::make_shared<SImplementation::ToSaveStop>(DImplementation -> all_stops[index]);
     }
     return nullptr;
 }
@@ -203,9 +296,9 @@ std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByIndex(std::size_t index)
 // Returns the SStop specified by the stop id, nullptr is returned if id is
 // not in the stops
 std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByID(TStopID id) const noexcept {
-    for (const auto& stop : DImplementation -> all_stops) {
-        if (all_stops -> ID() == id) {
-            return all_stops;
+    for (const auto& StopByID_all_stops  : DImplementation -> all_stops) {
+        if (StopByID_all_stops.ID() == id) {
+            return std::make_shared<SImplementation::ToSaveStop>(StopByID_all_stops);
         }
     }
     return nullptr;
@@ -219,7 +312,7 @@ std::shared_ptr<CBusSystem::SStop> CCSVBusSystem::StopByID(TStopID id) const noe
 
 std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByIndex(std::size_t index) const noexcept {
     if (index < DImplementation -> all_routes.size()) {
-        return DImplementation -> all_routes[index];
+        return std::make_shared<SImplementation::ToSaveRoute>(DImplementation -> all_routes[index]);
     }
     return nullptr;
 }
@@ -234,10 +327,15 @@ std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByIndex(std::size_t inde
 // not in the routes
 
 std::shared_ptr<CBusSystem::SRoute> CCSVBusSystem::RouteByName(const std::string &name) const noexcept {
-    for (const auto& route : DImplementation -> all_routes) {
-        if (all_routes -> Name() == name) {
-            return all_routes;
+    for (const auto& RouteByName_all_routes : DImplementation -> all_routes) {
+        // if (all_routes -> Name() == name) {
+        //     return all_routes;
+        // }
+
+        if (RouteByName_all_routes.route_name == name) {
+            return std::make_shared<SImplementation::ToSaveRoute>(RouteByName_all_routes);
         }
+
     }
     return nullptr;
 }
